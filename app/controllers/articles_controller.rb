84115053a3908022a00c32c4e4ff3cc16bb5c269
yashwanth <yashwanthreddy26@gmail.com>
2019-09-11 15:40:42 +0530

    new, show,edit and index

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,15 @@
 class ArticlesController < ApplicationController
+
+  def index
+    @articles = Article.all
+  end
+
   def new
     @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
   end
 
   def create
@@ -13,11 +22,20 @@ class ArticlesController < ApplicationController
     else
       render 'new'
     end
+  end
 
-    def show
-      @article = Article.find(params[:id])
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article is Updated"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
     end
-
   end
 
   private
